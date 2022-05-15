ad9a9e7df1d3127759294d5039fa541d3b28bc66
Facu TG <Facu TG@DESKTOP-SAMSUNG>
2022-05-15 17:44:31 -0300

    Alumnos:

---
+++ TP5_Grupo12/src/main/java/ar/edu/unju/fi/controller/AlumnosController.java
@@ -0,0 +1,52 @@
+package ar.edu.unju.fi.controller;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import ar.edu.unju.fi.model.Alumno;
+import ar.edu.unju.fi.util.ListaAlumno;
+
+@Controller
+@RequestMapping("/alumno")
+public class AlumnosController {
+	private static final Log LOGGER = LogFactory.getLog(AlumnosController.class);
+	
+	@GetMapping("/nuevo")
+	public String getFormNuevoAlumnoPage(Model model) {
+		model.addAttribute("alumno", new Alumno());
+		return "nuevo_alumno";
+	}
+	
+	@PostMapping("/guardar")
+	public ModelAndView getListaAlumnosPage(@ModelAttribute("alumno")Alumno alumno) {
+		ModelAndView mavalumno = new ModelAndView("mostrar_alumnos");
+		ListaAlumno listaAlumnos = new ListaAlumno();
+		if (listaAlumnos.getAlumnos().add(alumno)) {
+			LOGGER.info("Se agregó un objeto al arrayList Alumnos");
+		}
+		mavalumno.addObject("alumno", listaAlumnos.getAlumnos());
+		return mavalumno; 
+	}
+	
+	@GetMapping("/mostrar")
+	public String getAlumnosPage(Model model) {
+		ListaAlumno listaAlumnos = new ListaAlumno();
+		model.addAttribute("alumno", listaAlumnos.getAlumnos());
+		return "mostrar_alumnos";
+	}
+	
+	@GetMapping("/lista")
+	public String getListaAlumnosPage(Model model) {
+		ListaAlumno listaAlumnos = new ListaAlumno();
+		model.addAttribute("alumno", listaAlumnos.getAlumnos());
+		return "lista_alumnos";
+	}
+	
+}
---
+++ TP5_Grupo12/src/main/java/ar/edu/unju/fi/util/ListaAlumno.java
@@ -0,0 +1,22 @@
+package ar.edu.unju.fi.util;
+
+import java.util.ArrayList;
+
+import ar.edu.unju.fi.model.Alumno;
+
+public class ListaAlumno {
+	private ArrayList<Alumno> alumnos ;
+	
+	public ListaAlumno() {
+		alumnos = new ArrayList<Alumno>();
+		alumnos.add(new Alumno(33444555,"Juan","Perez","juanp@gmail.com","3884999777"));
+	}
+
+	public ArrayList<Alumno> getAlumnos() {
+		return alumnos;
+	}
+
+	public void setDocentes(ArrayList<Alumno> alumnos) {
+		this.alumnos = alumnos;
+	}
+}

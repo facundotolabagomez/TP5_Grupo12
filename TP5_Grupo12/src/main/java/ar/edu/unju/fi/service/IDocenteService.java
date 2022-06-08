ac431a9dd6d0ff9214e65f93da03a3a0b2cb58ee
Facu TG <Facu TG@DESKTOP-SAMSUNG>
2022-06-07 23:28:11 -0300

    Service Docente

---
+++ TP5_Grupo12/src/main/java/ar/edu/unju/fi/service/IDocenteService.java
@@ -0,0 +1,14 @@
+package ar.edu.unju.fi.service;
+
+import ar.edu.unju.fi.model.Docente;
+import ar.edu.unju.fi.util.ListaDocente;
+
+public interface IDocenteService {
+
+	public Docente getDocente();
+	public boolean guardarDocente(Docente docente);
+	public void modificarDocente (Docente docente);
+	public void eliminarDocente (int legajo);
+	public ListaDocente getListaDocente();
+	public Docente buscarDocente (int legajo);
+}
